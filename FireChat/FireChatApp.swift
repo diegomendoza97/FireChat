663fa1c32d72c6b98eab1aa6a3f2130f60e51319
Diego Mendoza <Diego Mendoza>
2022-05-23 22:36:44 -0700

    Initial Commit

---
+++ FireChat/FireChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireChatApp.swift
+//  FireChat
+//
+//  Created by Diego Mendoza on 5/3/22.
+//
+
+import SwiftUI
+
+@main
+struct FireChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
